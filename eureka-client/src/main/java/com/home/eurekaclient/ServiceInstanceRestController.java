84d8df727c76ba1c2a110fb9312aa6b12aaaa82e
“milindadev” <msck.dev@gmail>
2021-02-07 01:29:02 +0400

    service registry and discovery

---
+++ eureka-client/src/main/java/com/home/eurekaclient/ServiceInstanceRestController.java
@@ -0,0 +1,24 @@
+package com.home.eurekaclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import java.util.List;
+
+
+@RestController
+public class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstanceByApplicationName(
+            @PathVariable String applicationName
+    ) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
